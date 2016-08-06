fb494569bd555f96ce362a9f19779f0416d1f5b5
Dmitry Ponyatov <dponyatov@gmail.com>
2016-08-06 20:28:07 +0400

    + unify check
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- py.py
+++ py.py
@@ -13,13 +13,29 @@ class UnifyingVariable:
             yield False
 
 def personWithUnify(Person):
-    for l1 in Person.unify("Chelsea"): yield False
-    for l1 in Person.unify("Hillary"): yield False
-    for l1 in Person.unify("Bill"): yield False
+    for l1 in Person.unify("Chelsea"):
+        yield False
+    for l1 in Person.unify("Hillary"): # Person = "Hillary"
+        yield False
+    for l1 in Person.unify("Bill"):
+        yield False
 
+# def main():
+#     print("Names using UnifyingVariable:")
+#     Person = UnifyingVariable()
+#     for l1 in personWithUnify(Person):
+#         print(Person._value)
 def main():
-    print("Names using UnifyingVariable:")
+    print("Use unify to check a person:")
     Person = UnifyingVariable()
-    for l1 in personWithUnify(Person):
-        print(Person._value)
+    for l1 in Person.unify("Hillary"):
+        for l2 in personWithUnify(Person):
+            print("Hillary is a person.")
+    for i0 in personWithUnify(Person):      # personWithUnify(Person)
+        for i1 in Person.unify("Hillary"):  # Person = "Hillary"
+            print i0,i1,Person
+    for l1 in Person.unify("Buddy"):
+        for l2 in personWithUnify(Person):
+            # This won't print.
+            print("Buddy is a person.")
 main()
